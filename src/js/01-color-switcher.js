9731d13740a6727107989c338dd94917d45a6bd4
AlexandrIshchenko <98355714+AlexandrIshchenko@users.noreply.github.com>
2023-04-08 20:01:50 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,3 @@
-// const startBtn = document.querySelector("[data-start]");
-// const stopBtn = document.querySelector("[data-stop]");
-// const body = document.querySelector("body");
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-// };
-
-// startBtn.addEventListener("click", () => {
-//     console.log("start");
-// });
-
-// setTimeout(() => {
-    
-// }, 1000);
-
 const body = document.querySelector('body');
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
