4a65891bf8532bd23f77e5fb8b2620d860223c1f
AlexandrIshchenko <98355714+AlexandrIshchenko@users.noreply.github.com>
2023-04-08 19:16:45 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,51 @@
+// const startBtn = document.querySelector("[data-start]");
+// const stopBtn = document.querySelector("[data-stop]");
+// const body = document.querySelector("body");
 
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+// };
+
+// startBtn.addEventListener("click", () => {
+//     console.log("start");
+// });
+
+// setTimeout(() => {
+    
+// }, 1000);
+
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+const INTERVAL_DELAY = 1000;
+let intervalId = null;
+let btnIsActive = (startBtn.disabled = false);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+function onStartClick() {
+    if (btnIsActive) {
+    return;
+    }
+
+    intervalId = setInterval(callBackSetInterval, INTERVAL_DELAY);
+
+    btnIsActive = (startBtn.disabled = true);
+};
+
+function onStopClick() {
+    clearInterval(intervalId);
+    btnIsActive = (startBtn.disabled = false);
+}
+
+function callBackSetInterval() {
+    return (body.style.backgroundColor = getRandomHexColor());
+}
